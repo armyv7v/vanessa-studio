93586563f16c346a85e733438ebdb0efdffe02b2
armyv7v <enderjpinar@gmail.com>
2025-08-19 08:32:07 -0400

    act

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Desplegando sitio estático..."
 # Desplegar los archivos estáticos generados con el nombre del proyecto
-npx wrangler pages deploy .vercel/output/static --project-name=vanessa-nails
+npx wrangler pages deploy .vercel/output/static --project-name=vanessanails
 echo "Despliegue completado."
