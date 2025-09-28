462c72b3c02054f79a4d8d03682a107284f819da
armyv7v <enderjpinar@gmail.com>
2025-09-28 09:48:19 -0300

    Fix: Simplify and correct PWA configuration for Edge

--- next.config.js
+++ next.config.js
@@ -1,26 +1,21 @@
 ﻿﻿/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  // disable: process.env.NODE_ENV === 'development', // We will handle this manually
-  // register: true,
-  // skipWaiting: true,
-  // runtimeCaching: [], // Add this if you have specific caching needs
-  buildExcludes: [/middleware-manifest.json$/], // Exclude middleware manifest from service worker
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = {
-      ...(config.resolve.fallback || {}),
-      fs: false, net: false, tls: false, child_process: false,
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
     };
     return config;
   },
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
 };
 
-module.exports = process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig);
+module.exports = withPWA(nextConfig);
