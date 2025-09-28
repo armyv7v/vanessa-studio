96d93d1eae6355bc8fa338d13f1216681a1518e7
armyv7v <enderjpinar@gmail.com>
2025-09-28 09:55:54 -0300

    Fix: Configuración definitiva de PWA para Cloudflare

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 ﻿﻿/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development'
 });
 
 const nextConfig = {
@@ -18,4 +18,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
