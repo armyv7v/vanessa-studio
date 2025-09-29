c9c57a06a913acf2ae33391b17efa61f53c08f2b
armyv7v <enderjpinar@gmail.com>
2025-09-29 09:35:59 -0300

    Fix: Restaurar configuración de Tailwind CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,5 @@
-﻿import "../styles/globals.css";
-import { useEffect } from 'react';
+﻿﻿import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(
-          (registration) => console.log('Service Worker registration successful with scope: ', registration.scope),
-          (err) => console.log('Service Worker registration failed: ', err)
-        );
-      });
-    }
-  }, []);
   return <Component {...pageProps} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 ﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
-  plugins: []
-};
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
